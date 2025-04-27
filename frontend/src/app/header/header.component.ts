e1a63e5b065e238dcd338832141265542da7dd7b
timon483 <timon483@gmail.com>
2025-04-27 11:25:03 +0200

    feat(TAP-0003): Define homepage and header

---
+++ frontend/src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
+import {MatIconButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    MatMenuTrigger,
+    MatMenu,
+    MatMenuItem,
+    MatIconButton,
+    MatIcon
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MatButton} from '@angular/material/button';
+import {HeaderComponent} from '../header/header.component';
 
 @Component({
   selector: 'app-homepage',
   imports: [
-    MatButton
+    MatButton,
+    HeaderComponent
   ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
