75c0f521234c7ee91becbed01d4a1b0162fcb09b
timon483 <timon483@gmail.com>
2025-04-27 12:03:10 +0200

    feat(TAP-0003): add images

--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {MatButton} from '@angular/material/button';
 import {HeaderComponent} from '../header/header.component';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-homepage',
   imports: [
     MatButton,
-    HeaderComponent
+    HeaderComponent,
+    NgOptimizedImage
   ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
